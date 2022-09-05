c56590de3ab5a4bf4fbb2722eb1a1a3b3dfeed76
mtamerb <tamerbilici00@gmail.com>
2022-09-05 17:39:45 +0300

    Fixed

---
+++ README.md
@@ -0,0 +1,38 @@
+# Assignment - Book Sorter
+## Book Sorter
+
+
+Design a class called Book. Inherit from this class Comparable interface and override the "compareTo" method. Inside this method, write the code that sorts the book by name from A to Z. Create 5 objects of this class and store the objects in a Set type structure. Then use a Set type data structure a second time and have it sort the books by page count.
+
+
+
+The Book class consists of the name of the book, the number of pages, the name of the author, and the date of publication.
+
+![](image/markdown.png)
+>Örnek Proje Resmi
+
+
+## Installation
+
+Öncelikle projeyi klonlayın. 
+
+```bash
+git clone https://github.com/mtamerb/assignment-booksorter.git
+```
+
+
+
+## Usage
+
+Projeyi klonladıktan sonra Intellij Idea programında açınız.
+
+
+
+## Contributing
+
+Pull requestler kabul edilir. Büyük değişiklikler için, lütfen önce neyi değiştirmek istediğinizi tartışmak için bir konu açın.
+
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/Main.java
+++ src/Main.java
@@ -12,17 +12,17 @@ public class Main {
         // 1.sorts the book by name from A to Z.
         TreeSet<Book> bookName = new TreeSet<Book>();
 
-        bookName.add(new Book("Ben robot", 248, "Isaac Asimov",
-                "Aralık 1950"));
+        bookName.add(new Book("I Robot\t", 248, "Isaac Asimov",
+                " 1951"));
 
-        bookName.add(new Book("Vakıf\t", 304, "Isaac Asimov",
-                "Mayıs 1942"));
+        bookName.add(new Book("Faundation", 304, "Isaac Asimov",
+                " 1942"));
 
         bookName.add(new Book("Dune\t", 712, "Frank Herbert",
-                "Ağustos 1965"));
+                " 1965"));
 
-        bookName.add(new Book("Kozmos\t", 100, "Carl Sagan",
-                "\tEkim 1980"));
+        bookName.add(new Book("Cosmos\t", 100, "Carl Sagan",
+                "\t 1980"));
 
 
         System.out.println("// 1.sorts the book by name from A to Z.");
@@ -37,17 +37,17 @@ public class Main {
         // 2.sorts the book by pages from 1 to N.
         TreeSet<ComparatorSecond> bookPages = new TreeSet<ComparatorSecond>();
 
-        bookPages.add(new ComparatorSecond("Ben Robot", 248, "Isaac Asimov",
-                "Aralık 1950"));
+        bookPages.add(new ComparatorSecond("I Robot\t", 248, "Isaac Asimov",
+                "1951"));
 
-        bookPages.add(new ComparatorSecond("Vakıf\t", 304, "Isaac Asimov",
-                "Mayıs 1942"));
+        bookPages.add(new ComparatorSecond("Faundation", 304, "Isaac Asimov",
+                "1952"));
 
         bookPages.add(new ComparatorSecond("Dune\t", 712, "Frank Herbert",
-                "Ağustos 1965"));
+                "1965"));
 
-        bookPages.add(new ComparatorSecond("Kozmos\t", 100, "Carl Sagan",
-                "\tEkim 1980"));
+        bookPages.add(new ComparatorSecond("Cosmos\t", 100, "Carl Sagan\t",
+                "1980"));
 
 
         System.out.println("\n// 2.sorts the book by pages from 1 to N.");
